91425d6deedcf82c4d12428522ac56f76fbd8849
unknown <omavaloss@gmail.com>
2018-06-08 15:40:41 -0500

    El primero

---
+++ config.js
@@ -0,0 +1,90 @@
+module.exports = {
+    "casio":{
+        "storeId": "277967455",
+        "token"  : "TEST-3447990366340016-020814-c419872ade0f0cf09ae2837d3188c79c__LC_LD__-277967455"
+    },
+    "anfora":{
+        "storeId": "263297351",
+        "token"  : "TEST-885826076823900-020916-80e2c83c4d810384741122f9397b5a8a__LA_LD__-263297351"
+    },
+    "babyliss":{
+        "storeId": "273345704",
+        "token"  : "TEST-6886438812232901-112416-cbf643db834da1becef12c1982b0cf24__LC_LA__-273345704"
+    },
+    "brazil":{
+        "storeId": "257677615",
+        "token"  : "TEST-6546664645452342-062520-aca948713b6989f5c30b30aa3cef67d0__LA_LB__-257677615"
+    },
+    "conair":{
+        "storeId": "246172619",
+        "token"  : "TEST-5577699639564912-111712-2626b132944c321b64659bda9d967f69__LC_LA__-246172619"
+    },
+    "cuisinart":{
+        "storeId": "268037201",
+        "token"  : "TEST-3113081621872651-112804-308a9936db44e2fd6daf031eabd4a4c3__LD_LA__-268037201"
+    },
+    "delsey":{
+        "storeId": "236810151",
+        "token"  : "TEST-3194519136008266-120417-5a51d10761d81e0d9fdf0dfab7a2fe4f__LA_LD__-236810151"
+    },
+    "delsey":{
+        "storeId": "236810151",
+        "token"  : "TEST-3194519136008266-120417-5a51d10761d81e0d9fdf0dfab7a2fe4f__LA_LD__-236810151"
+    },
+    "generra":{
+        "storeId": "256632302",
+        "token"  : "TEST-6811195002300851-102315-bcf8552a06feccd6159f50e93e3de310__LC_LB__-256632302"
+    },
+    "leon":{
+        "storeId": "262077576",
+        "token"  : "TEST-1039046571696529-121112-f876bdd377c1b2e7b30eabf86965017c__LB_LA__-262077576"
+    },
+    "calza-market":{
+        "storeId": "289201235",
+        "token"  : "TEST-8759104096300416-020813-e2084470e8a8addb14ef49b80d4910b4__LB_LA__-289201235"
+    },
+    "prohat":{
+        "storeId": "278947713",
+        "token"  : "TEST-2802368248522676-030916-f97f03fb1cb71b36ab681f4709337dcb__LD_LB__-278947713"
+    },
+    "seire":{
+        "storeId": "191225181",
+        "token"  : "TEST-6415677547772960-012318-e91ccc087e1f67b484885201706ae485__LD_LA__-191225181"
+    },
+    "discovery":{
+        "storeId": "268110859",
+        "token"  : "TEST-6108949757184982-051618-ed4ac234bbe6c601feb844e83616c212-268110859"
+    },
+    "swissbrand":{
+        "storeId": "268086193",
+        "token"  : "TEST-3516580537027879-051813-845cf050083daa2a6dc0a0b6df9a5dcd-268086193"
+    },
+    "argos":{
+        "storeId": "302275966",
+        "token"  : "TEST-1646158439924483-031614-b117447ee8197b3978b2f8faa86f8db9-302275966"
+    },
+    "lightincap":{
+        "storeId": "312680153",
+        "token"  : "TEST-4399678479188836-043017-25cd7e2d7830ebd6fa5bb9851c28ce3c-312680153"
+    },
+    "polyllantas":{
+        "storeId": "53574607",
+        "token"  : "TEST-1095504243189025-101708-a3c1f1455fa0e2122e8c4d1f7397b1dc__LC_LD__-53574607"
+    },
+    "springair":{
+        "storeId": "283157350",
+        "token"  : "TEST-6979794014880084-112916-97d0f89fa23cbcea11fc34d7a2654633__LB_LA__-283157350"
+    },
+    "evolucion":{
+        "storeId": "142830882",
+        "token"  : "TEST-7603660332148613-061515-e83f1f5aeb4f9e78d49dbadcc071dcc5__LD_LC__-142830882"
+    },
+    "berrendo":{
+        "storeId": "316731396",
+        "token"  : "TEST-7208431149852041-051810-8c518c548dff3ea5600be05867e8136a-316731396"
+    },
+    "fox-racing":{
+        "storeId": "140112203",
+        "token"  : "TEST-2749670348975196-053115-e080c4f452b4588774f61e49cb238ca3-140112203"
+    }
+}
---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const request = require('request');
+const rp      = require('request-promise');
+const config  = require('./config');
+const app = express();
+
+const baseURL= 'https://api.mercadoshops.com/v1/shops';
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('BEIA Y BONITA API');
+})
+
+/*
+---versión ideal:
+app.get('/api/v1/ordenes/:marca', (req,res)=>{
+
+})
+*/
+
+app.get('/:marca', (req,res)=>{
+
+    request(`${baseURL}/${config[req.params.marca].storeId}/orders/search?access_token=${config[req.params.marca].token}`, (err, response, body) => {
+        let b = JSON.parse(body);
+        let total = b.paging.total;
+        let paging = total/50;
+        let pagingTotal = paging.toFixed(0);
+        console.log(pagingTotal)
+        resultados = [];
+        promesas = [];
+
+        if(total%50 !== 0 || total%50 == 0){
+            let offset = 0;
+
+            //iteramos en la paginación
+            for(let i = 0; i<=pagingTotal; i++ ){
+               console.log("página:" +i)
+               //hacemos get y lo almacenamos en variable
+               let peticion =  rp.get(`${baseURL}/${config[req.params.marca].storeId}/orders/search?access_token=${config[req.params.marca].token}&offset=${offset}&limit=50`)
+               //guardamos esa variable en un arreglo
+                promesas.push(peticion);
+                //subimos el offset
+                offset += 50;
+            }
+        }
+
+        //cuando ya acabó de hacer peticiones ejecutamos el promise.all
+        //esto nos da la posibilidad de procesar los datos de todos los get juntos :D
+        Promise.all(promesas)
+            .then( (promesa) => {
+                let arr = [];
+                let r = promesa.map( (p, index) => {
+                    let parce = JSON.parse(p);
+                    return parce.results;
+                })
+
+                //convertimos el arreglo de dos dimensiones en una dimensión
+                //iteramos las veces necesarias en el arreglo principal
+                //luego iteramos en el arreglo dentro del arreglo para hacerle push a arr y sacar lo que hay dentro (if you know what i mean)
+                for(let j = 0; j<r.length; j++){
+                    for(let k =0; k<r[j].length; k++){
+                        arr.push(r[j][k]);
+                    }
+
+                }
+
+                console.log(arr.length)
+                res.send(arr)
+            })
+    })
+})
+
+app.listen(3030, () =>{
+    console.log('Oh, hi Mark');
+})
